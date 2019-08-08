9e5d344dc9dd1e2d4f2cbc47ecce9aea8f258c89
JustDoIt521 <zhbzwayzhawfbyg@gmail.com>
2019-08-08 20:41:54 +0800

    2019-08-08次提交

--- element-coding/src/plugins/packages/el-test-radio/radio-group.vue
+++ element-coding/src/plugins/packages/el-test-radio/radio-group.vue
@@ -2,7 +2,7 @@
   <div
     class="el-radio-group"
     role="radioGroup"
-    @keydown="handleKeydown"
+    @keydown="handleKeyDown"
   >
     <slot></slot>
   </div>
@@ -20,6 +20,13 @@ export default {
   name: "ElTestRadioGroup",
 
   componentName: "ElTestRadioGroup",
+
+  // 集成自el-form的属性
+  inject: {
+    elFormItem: {
+      default: ''
+    }
+  },
 
   mixins: [Emitter],
 
@@ -42,6 +49,7 @@ export default {
   },
 
   created() {
+    // 定义一个监听函数
     this.$on("handleChange", value => {
       this.$emit("change", value);
     });
@@ -50,25 +58,31 @@ export default {
   mounted() {
     // 当radioGroup没有默认选项时，第一个可以选中Tab导航
     const radios = this.$el.querySelectorAll("[type=radio]");
-    const firstLabel = this.$el.querySelectorAll("[type=radio]")[0];
+    const firstLabel = this.$el.querySelectorAll("[role=radio]")[0];
     if (![].some.call(radios, radio => radio.checked) && firstLabel) {
       firstLabel.tabIndex = 0;
+      console.warn(1)
     }
   },
 
   methods: {
-    handleKeydown(e) {
-      // 左右上下按键 可以在radio组内切换不同选项
-      const target = e.target;
-      const className = target.nodeName === "INPUT" ? "[type=radio]" : "[role=radio]";
+    // 上下左右按键 触发radio
+    handleKeyDown (e) {
+      // 获取触发该事件的节点
+      const  target = e.target;
+      // 判断是否是原生input节点
+      const className = target.nodeName === 'INPUT' ? '[type=radio]' : '[role=radio]'
       const radios = this.$el.querySelectorAll(className);
       const length = radios.length;
-      const index = [].indexOf.call(radios, target);
-      const roleRadios = this.$el.querySelectorAll("[role=radio]");
+      const index = [].indexOf.call(radios, target)
+      const roleRadios = this.$el.querySelectorAll('[role=radio]')
       switch (e.keyCode) {
         case keyCode.LEFT:
         case keyCode.UP:
+        // (删除这两个事件好像没什么变化)
+          // 阻止事件的传播 阻止该事件配分派到其他节点上 
           e.stopPropagation();
+          //如果此事件没有被显式处理，那么它默认的动作也不要做（因为默认是要做的）。此事件还是继续传播，除非碰到事件侦听器调用stopPropagation() 或stopImmediatePropagation()，才停止传播
           e.preventDefault();
           if (index === 0) {
             roleRadios[length - 1].click();
@@ -80,7 +94,7 @@ export default {
           break;
         case keyCode.RIGHT:
         case keyCode.DOWN:
-          if (index === length - 1) {
+          if (index === (length - 1)) {
             e.stopPropagation();
             e.preventDefault();
             roleRadios[0].click();
@@ -97,8 +111,8 @@ export default {
   },
 
   watch: {
-    value() {
-      // this.dispatch('ElFormItem', 'el.form.change', [this.value])
+    value () {
+      this.dispatch('ElformItem', 'el.form.change', [this.value])
     }
   }
 };
--- element-coding/src/views/Home.vue
+++ element-coding/src/views/Home.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="home">
-    <el-test-radio v-model="radio" label="1"></el-test-radio>
-    <el-test-radio v-model="radio" label="2"></el-test-radio>
-    <el-test-radio v-model="radio" label="3"></el-test-radio>
+    <el-test-radio-group v-model="radio">
+    <!-- <el-test-radio -->
+      <el-test-radio label="1"></el-test-radio>
+      <el-test-radio label="2"></el-test-radio>
+      <el-test-radio label="3"></el-test-radio>
+    </el-test-radio-group>
 
+    <el-radio-group v-model="radio1">
+    <!-- <el-test-radio -->
+      <el-radio label="6"></el-radio>
+      <el-radio label="7"></el-radio>
+      <el-radio label="8"></el-radio>
+    </el-radio-group>
     <input :value="radio" :input="changeInput" type="radio">
   </div>
 </template>
@@ -19,11 +28,18 @@ export default {
     return {
       gutter: 0,
       radio: '4',
+      radio1: '',
       inputValue: '222',
       radioValue: ''
     }
   },
   mounted() {
+    // const radios = this.$el.querySelectorAll('[type=radio]');
+    // console.log(radios)
+    // let m1 = [1, 2, 3, 4, 5]
+    // let m2 = [6, 7, 8, 9, 10]
+    // let test = m1.some.call([], value => value === 9)
+    // console.warn(test)
     // console.warn(this.$children)
     // console.warn(this.$children[0].value)
   },
